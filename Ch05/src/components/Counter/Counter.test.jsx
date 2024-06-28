f00f8341331af3558c48cf088ffbf21739658ee0
Jasper Wong <wlcjasper@gmail.com>
2024-06-28 14:27:51 +0800

    Add Counter GUI and test

--- Ch05/src/components/Counter/Counter.test.jsx
+++ Ch05/src/components/Counter/Counter.test.jsx
@@ -10,7 +10,7 @@ test('The default text display in view will be 目前數字：0', () => {
   const { getByText } = render(<Counter />);
  
   // Act
-  expect(getByText('目前數字：0')).toBeInTheDocument();
+  expect(getByText('目前數字：1')).toBeInTheDocument();
 });
 
 test('The text of count display in view will from 0 change to 1 after I clicked 點我加一 button', () => {
--- Ch05/src/index.jsx
+++ Ch05/src/index.jsx
@@ -1,29 +1,29 @@
- import 'regenerator-runtime/runtime';
-import 'core-js/stable'; 
-import React from 'react';
-import ReactDOM from 'react-dom'; 
-import { HashRouter, Switch, Route } from 'react-router-dom'; 
-import { Provider } from 'react-redux'; 
-import store from './store';
-import Home from './views/Home';
-import About from './views/About'; 
-import Menu from './components/Menu'; 
-import NotFound from './views/NotFound'; 
-import News from './views/News';
+import "regenerator-runtime/runtime"
+import "core-js/stable"
+import React from "react"
+import ReactDOM from "react-dom"
+import { HashRouter, Switch, Route } from "react-router-dom"
+import { Provider } from "react-redux"
+import store from "./store"
+import Home from "./views/Home"
+import About from "./views/About"
+import Menu from "./components/Menu"
+import Counter from "./components/Counter"
+import NotFound from "./views/NotFound"
+import News from "./views/News"
 
-
- 
-ReactDOM.render( 
+ReactDOM.render(
   <Provider store={store}>
     <HashRouter>
       <Menu />
       <Switch>
         <Route path="/news" component={News} />
         <Route path="/home" component={Home} />
-        <Route path="/about" component={About} />  
+        <Route path="/about" component={About} />
+        <Route path="/counter" component={Counter} />
         <Route path="/" component={NotFound} />
       </Switch>
     </HashRouter>
   </Provider>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
